ff8ad8aff05fd51f5a1e49468d02f2b802808c30
rbaker4559 <reginaldsbaker10@icloud.com>
2024-01-30 19:01:51 -0800

    adding additional time blocks on html; adding event listener on js

--- Develop/script.js
+++ Develop/script.js
@@ -12,42 +12,42 @@ $(document).ready(function () {
   })
 
   //validates data by comparing current time w/ various time blocks; assigns class once validated
-  function updateHours() {
-    var currentHour = dayjs().hour();
-    $(".time-block").each(function() {
-      var timeBlock = $(this);
-      var timeBlockHour = parseInt(timeBlock.attr("id").split("-")[1]);
+  // function updateHours() {
+  //   var currentHour = dayjs().hour();
+  //   $(".time-block").each(function() {
+  //     var timeBlock = $(this);
+  //     var timeBlockHour = parseInt(timeBlock.attr("id").split("-")[1]);
 
-      if (timeBlockHour < currentHour) {
-        timeBlock.removeClass("present future").addClass("past");
-      } else if (timeBlockHour === currentHour){
-        timeBlock.removeClass("past future").addClass("present");
-      } else if (timeBlockHour > currentHour){
-        timeBlock.removeClass("past present").addClass("future");
-      }
-    });
-  }
+  //     if (timeBlockHour < currentHour) {
+  //       timeBlock.removeClass("present future").addClass("past");
+  //     } else if (timeBlockHour === currentHour){
+  //       timeBlock.removeClass("past future").addClass("present");
+  //     } else if (timeBlockHour > currentHour){
+  //       timeBlock.removeClass("past present").addClass("future");
+  //     }
+  //   });
+  // }
   
 
   //saves associated textarea elements
-  $(".time-block").each(function(){
-    var timeBlock = $(this);
-    var timeBlockID = timeBlock.attr("id");
-    var savedEvent = localStorage.getItem(timeBlockID)
+  // $(".time-block").each(function(){
+  //   var timeBlock = $(this);
+  //   var timeBlockID = timeBlock.attr("id");
+  //   var savedEvent = localStorage.getItem(timeBlockID)
 
-    if(savedEvent) {
-      timeBlock.find(".description").val(savedEvent);
-    }
-  })
+  //   if(savedEvent) {
+  //     timeBlock.find(".description").val(savedEvent);
+  //   }
+  // })
   
   
   //displays current date w/in header
-  function displayDate() {
-    var currentDate = dayjs().format("dddd, MMMM, D, YYYY");
-    $("#currentDay").text(currentDate);
-  }
-  updateHours();
-  displayDate();
-  setInterval(displayDate(), 60000);
+  // function displayDate() {
+  //   var currentDate = dayjs().format("dddd, MMMM, D, YYYY");
+  //   $("#currentDay").text(currentDate);
+  // }
+  // updateHours();
+  // displayDate();
+  // setInterval(displayDate(), 60000);
 
 });
